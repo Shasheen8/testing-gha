9194c183e467a1eae793e38c30abd6e7533cde5b
Shasheen_B <50681308+Shasheen8@users.noreply.github.com>
2025-06-01 22:45:55 -0700

    Revert "feat: testing gha for vuln code"

--- main.py
+++
@@ -1,9 +0,0 @@
-import yaml
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/load")
-def unsafe_yaml():
-    data = "!!python/object/apply:os.system ['ls']"
-    return yaml.load(data, Loader=yaml.Loader) 
--- new_vuln.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-from flask import Flask, request
-
-app = Flask(__name__)
-
-@app.route("/ping")
-def ping():
-    host = request.args.get("host")
-    return subprocess.check_output(f"ping -c 1 {host}", shell=True)
