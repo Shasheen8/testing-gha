8f4515f216cbfad3340727f143615b010864738b
Shasheen_B <shasheenbandodkar98@gmail.com>
2025-06-01 21:42:55 -0700

    refactor: added secrets

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ import yaml
 @app.route("/yaml")
 def vuln():
     return yaml.load("!!python/object/apply:os.system ['ls']", Loader=yaml.Loader)  # unsafe
+
+
+
+AWS_SECRET_ACCESS_KEY=AKIAIOSFODNN7EXAMPLE
+PRIVATE_KEY=-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBg...
+GITHUB_TOKEN=ghp_1234567890abcdefghijklmnopqrstuv
+slack_token=xoxb-123456789012-1234567890123-ABCDEFGHIJKLMNO
